4962eee3ca1d540f68d4027de5ca798f5462877a
selankon <selankon@selankon.xyz>
2024-11-08 16:12:42 +0100

    chore(mesh-config): refactor query keys

--- plugins/lime-plugin-mesh-wide-config/src/meshConfigQueries.tsx
+++ plugins/lime-plugin-mesh-wide-config/src/meshConfigQueries.tsx
@@ -1,8 +1,17 @@
-import { QueryKey, UseQueryOptions, useQuery } from "@tanstack/react-query";
+import {
+    UseMutationOptions,
+    UseQueryOptions,
+    useMutation,
+    useQuery,
+} from "@tanstack/react-query";
 
 import { doSharedStateApiCall } from "components/shared-state/SharedStateApi";
+import { sharedStateQueries } from "components/shared-state/SharedStateQueriesKeys";
 
-import { meshConfigQueryKeys } from "plugins/lime-plugin-mesh-wide-config/src/meshConfigQueriesKeys";
+import {
+    MeshConfigQueryKeys,
+    meshConfigStateKey,
+} from "plugins/lime-plugin-mesh-wide-config/src/meshConfigQueriesKeys";
 import {
     GetCommunityConfigResponse,
     IMeshWideConfig,
@@ -10,48 +19,82 @@ import {
     NodeMeshConfigInfo,
 } from "plugins/lime-plugin-mesh-wide-config/src/meshConfigTypes";
 import { parseConfigFile } from "plugins/lime-plugin-mesh-wide-config/src/utils/jsonParser";
+import { MeshWideRPCReturnTypes } from "plugins/lime-plugin-mesh-wide-upgrade/src/meshUpgradeTypes";
 
-import { standarizedApiCall } from "utils/standarizedApi";
+import {
+    ApiServiceParamsType,
+    StandarizedApiError,
+    standarizedApiCall,
+} from "utils/standarizedApi";
 
-export function useCommunityConfig(
+export const useCommunityConfig = (
     params?: Omit<
         UseQueryOptions<GetCommunityConfigResponse, Error, IMeshWideConfig>,
         "queryFn" | "queryKey"
     >
-) {
+) => {
     return useQuery<GetCommunityConfigResponse, Error, IMeshWideConfig>({
-        queryKey: meshConfigQueryKeys.getCommunityConfig(),
+        queryKey: MeshConfigQueryKeys.getCommunityConfig,
         queryFn: () =>
             standarizedApiCall<GetCommunityConfigResponse>({
-                args: meshConfigQueryKeys.getCommunityConfig(),
+                args: MeshConfigQueryKeys.getCommunityConfig,
             }),
         select: (data) => parseConfigFile(data.file_contents),
         ...params,
     });
+};
+
+interface SetCommunityConfigParams {
+    file_contents: string;
 }
 
-export function useMeshWideConfigState(
-    params?: Omit<UseQueryOptions<MeshWideConfigState>, "queryFn" | "queryKey">
-) {
-    return useQuery<MeshWideConfigState>({
-        queryKey: meshConfigQueryKeys.getMeshWideConfigInfo() as QueryKey,
-        queryFn: () =>
-            doSharedStateApiCall<"mesh_config">(
-                meshConfigQueryKeys.getMeshWideConfigInfo()
-            ),
+export const useSetCommunityConfig = (
+    params?: Omit<
+        UseMutationOptions<
+            MeshWideRPCReturnTypes,
+            StandarizedApiError,
+            SetCommunityConfigParams
+        >,
+        "mutationFn" | "mutationKey"
+    >
+) => {
+    return useMutation<
+        MeshWideRPCReturnTypes,
+        StandarizedApiError,
+        SetCommunityConfigParams
+    >({
+        mutationKey: MeshConfigQueryKeys.setCommunityConfig,
+        mutationFn: (args) => {
+            return standarizedApiCall<MeshWideRPCReturnTypes>({
+                args: [...MeshConfigQueryKeys.setCommunityConfig, {}],
+            });
+        },
         ...params,
     });
-}
+};
 
-export function useConfigNodeState(
+export const useMeshWideConfigState = (
+    params?: Omit<UseQueryOptions<MeshWideConfigState>, "queryFn" | "queryKey">
+) => {
+    const queryKey = sharedStateQueries.getFromSharedState(
+        meshConfigStateKey
+    ) as ApiServiceParamsType;
+    return useQuery<MeshWideConfigState>({
+        queryKey,
+        queryFn: () => doSharedStateApiCall<"mesh_config">(queryKey),
+        ...params,
+    });
+};
+
+export const useConfigNodeState = (
     params?: Omit<UseQueryOptions<NodeMeshConfigInfo>, "queryFn" | "queryKey">
-) {
+) => {
     return useQuery<NodeMeshConfigInfo>({
-        queryKey: meshConfigQueryKeys.getNodeStatus(),
+        queryKey: MeshConfigQueryKeys.getNodeStatus,
         queryFn: () =>
             standarizedApiCall<NodeMeshConfigInfo>({
-                args: meshConfigQueryKeys.getNodeStatus(),
+                args: MeshConfigQueryKeys.getNodeStatus,
             }),
         ...params,
     });
-}
+};
--- plugins/lime-plugin-mesh-wide-config/src/meshConfigQueriesKeys.tsx
+++ plugins/lime-plugin-mesh-wide-config/src/meshConfigQueriesKeys.tsx
@@ -1,32 +1,15 @@
-import { sharedStateQueries } from "components/shared-state/SharedStateQueriesKeys";
 import { MeshConfigTypes } from "components/shared-state/SharedStateTypes";
 
-import { ApiServiceParamsType } from "utils/standarizedApi";
-
-const MeshConfigQueryKeys: { [key: string]: ApiServiceParamsType } = {
+export const MeshConfigQueryKeys: {
+    [key: string]: [string, string];
+} = {
     getNodeStatus: ["lime-mesh-config", "get_node_status"],
-    getCommunityConfig: ["lime-mesh-config", "get_community_config", {}],
+    getCommunityConfig: ["lime-mesh-config", "get_community_config"],
     remoteConfirmUpgrade: ["lime-mesh-config", "start_config_transaction"],
     startSafeReboot: ["lime-mesh-config", "start_safe_reboot"],
     confirm: ["lime-mesh-config", "confirm"],
     remoteAbort: ["lime-mesh-config", "abort"],
+    setCommunityConfig: ["lime-mesh-config", "start_config_transaction"],
 };
 
 export const meshConfigStateKey: keyof MeshConfigTypes = "mesh_config";
-
-export const meshConfigQueryKeys = {
-    getMeshWideConfigInfo: (): ApiServiceParamsType =>
-        sharedStateQueries.getFromSharedState(
-            meshConfigStateKey
-        ) as ApiServiceParamsType,
-    getNodeStatus: (): ApiServiceParamsType =>
-        MeshConfigQueryKeys.getNodeStatus,
-    getCommunityConfig: (): ApiServiceParamsType =>
-        MeshConfigQueryKeys.getCommunityConfig,
-    remoteConfirmUpgrade: (): ApiServiceParamsType =>
-        MeshConfigQueryKeys.remoteConfirmUpgrade,
-    startSafeReboot: (): ApiServiceParamsType =>
-        MeshConfigQueryKeys.startSafeReboot,
-    confirm: (): ApiServiceParamsType => MeshConfigQueryKeys.confirm,
-    remoteAbort: (): ApiServiceParamsType => MeshConfigQueryKeys.remoteAbort,
-};
